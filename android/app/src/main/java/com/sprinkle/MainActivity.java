f0f8d553920b1a855f16c1fd64a4a569134af2a5
João Ricardo <jrflga@gmail.com>
2016-07-04 16:25:44 -0300

    Add Drawer and depedencies

--- android/app/src/main/java/com/sprinkle/MainActivity.java
+++ android/app/src/main/java/com/sprinkle/MainActivity.java
@@ -1,8 +1,10 @@
 package com.sprinkle;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,7 +36,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
-        );
+            new MainReactPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeMapboxGLPackage());
     }
 }
--- index.android.js
+++ index.android.js
@@ -1,58 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Mapbox from 'react-native-mapbox-gl';
+import ToolbarAndroid from 'ToolbarAndroid';
+import DrawerLayoutAndroid from 'DrawerLayoutAndroid';
+var mapRef = 'mapRef';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  ListView,
   View
 } from 'react-native';
 
-var ToolbarAndroid = require('ToolbarAndroid');
 
 class Sprinkle extends Component {
-  render() {
+
+    render() {
+      var navigationView = (
+          <View style={{flex: 1, backgroundColor: '#b3c87a'}}>
+              <View style={{height: 200, backgroundColor: '#347A2A'}}>
+              </View>
+              <View style={{height: 100, width: 100, backgroundColor: '#eee', borderRadius: 50, bottom: 50, left: 100, alignItems: 'center', justifyContent: 'center'}}>
+                  <Icon name="envira" size={50} color="#347A2Ag" />
+              </View>
+          </View>
+        );
+
     return (
     <View style={styles.container}>
-        <ToolbarAndroid
-        style={styles.toolbar}
-        title="Sprinkle"
-        titleColor="#202e24"
-        actions={[{title: 'Account'}]}
-        />
-        <View style={styles.map}>
-            <Text style={styles.mapText}>
-                Content
-            </Text>
-        </View>
-    </View>
+        <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}>
+            <Icon.ToolbarAndroid
+                style={styles.toolbar}
+                title="Sprinkle"
+                titleColor="#202e24"
+                actions={[{title: 'Account', iconName: 'user', iconColor: 'black', iconSize: 24, show: 'always'}]}
+            />
+            <Mapbox
+                accessToken={'pk.eyJ1IjoianJmbGdhIiwiYSI6ImNpcTg2bDNqejAwc3Vmc20xeWltOThvaTYifQ.yWNGTLAa4yCO2rppxdZwzA'}
+                centerCoordinate={{latitude: -22.850, longitude: -43.3044}}
+                debugActive={false}
+                direction={10}
+                ref={mapRef}
+                rotateEnabled={false}
+                scrollEnabled={true}
+                style={styles.container}
+                styleURL={"mapbox://styles/jrflga/ciq8donp7001jbkmbps2i7lmg"}
+                showsUserLocation={true}
+                zoomEnabled={true}
+                zoomLevel={13}
+                compassIsHidden={false}
+                attributionButtonIsHidden={true}
+            />
+        </DrawerLayoutAndroid>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ebe8be',
+    flex: 1
   },
   toolbar: {
     height: 56,
     backgroundColor: '#b3c87a',
-    elevation: 3
-  },
-  map: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 100,
-    backgroundColor: '#C4C193'
-  },
-  mapText: {
-    fontSize: 25,
-    fontFamily: 'monospace'
+    elevation: 2,
   }
 });
 
